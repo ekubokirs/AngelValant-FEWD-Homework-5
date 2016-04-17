bc321852fb62b5ab71cb01c2e44d44408e11bf4f
Angel Valant <angelgithub@gmail.com>
2016-04-17 18:57:32 -0400

    conditional statements
    added conditional statements for switching backgrounds

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
+//A user types in a string of text and clicks on the submit button
+//If the text aligns with any of the strings below it will trigger
+//the corresponding photo background stored in a class in the css file
+
 $(document).ready(function() {
+
+	//The button on the HTML document will start the js function "userCity"
+	$("#submit-btn").click(userCity);
 
 	function userCity() {
 		event.preventDefault();
 
-		var city = $('#city-type').val();
+		//The variable "city" will hold the user input from the textbox labeled "city-type"
+		var city = $("#city-type").val();
 
-		if (city == 'New York' || city == 'New York City' || city =='NYC') {
-			$('body').css('background-image', 'url(../images/nyc.jpg)');
+		//This is a conditional statement comparing the user input to a variety of text triggers
+		if(city == "NYC" || "New York" || "New York City") {
+			$("body").removeClass();
+			$("body").addClass("nyc");
+		} else if(city == "San Francisco" || "SF" || "Bay Area") {
+			$("body").removeClass();
+			$("body").addClass("sf");
+		} else if(city == "Los Angeles" || "LA" || "LAX") {
+			$("body").removeClass();
+			$("body").addClass("la");
+		} else if(city == "Austin" || "ATX") {
+			$("body").removeClass();
+			$("body").addClass("austin");
+		} else if(city == "Sydney" || "SYD") {
+			$("body").removeClass();
+			$("body").addClass("sydney");
 		}
-	}
 
-	$('#submit-btn').click(userCity);
+	}
 
 });
