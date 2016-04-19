6b865b8579f5020bbf88c5c3800f32563b4f6515
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2016-04-19 18:58:14 -0400

    added comments

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 
 		//The variable city takes the text input from city-type and switches it to lowercase text and removes any spacing before and after the input text
 		var city = $.trim($('#city-type').val().toLowerCase());
+		// Great job helping to normalize your inputs
 
 		//These are conditional statements comparing user input to a number of text inputs to generate a background.
 		//The previous class is removed and current class background added. The textbook is restored to its default.
@@ -43,6 +44,7 @@ $(document).ready(function() {
 			$('body').addClass('sydney');
 			$('#city-type').val('');
 		}
+		// It would be nice to but an else statement here too to catch the inputs that arent nice and pretty
 	}
 	
 
